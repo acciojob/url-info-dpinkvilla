f91e802da7868b131626ee66d61c2b2567847bbf
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-03-14 13:28:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 //your JS code here. If required.
-function URL_length(){
-    let current=location.href;
+// function URL_length(){
+//     let current=location.href;
 
-    let current2=current.split('');
-    return current2.length;
-}
-URL_length();
-alert("The length of the URL is:"+URL_length());
+//     let current2=current.split('');
+//     return current2.length;
+// }
+// URL_length();
+// alert("The length of the URL is:"+URL_length());
+let current=location.href;
+
+let current2=current.split('');
+// 
+// let current3=current2.length;
+// console.log(current3);
+alert("The length of the URL is:"+current2.length);
