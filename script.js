9058b7e9138fa22f246f7885f71898b49af2c094
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-03-14 13:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function URL_length(){
+    let current=location.href;
+
+    let current2=current.split('');
+    return current2.length;
+}
+URL_length();
+alert("The length of the URL is:"+URL_length());
